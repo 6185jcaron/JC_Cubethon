cb9a4ad4a45c0a45d8c0e8af1e5cebe66382a702
6185jcaron <123214297+6185jcaron@users.noreply.github.com>
2023-03-09 16:03:55 -0700

    added a script

---
+++ Cubethon/Assets/Scripts/Playermovement.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+public class Playermovement : MonoBehaviour
+   
+{
+    public Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(0, 200, 500);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
