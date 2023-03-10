7742a94a4a8cbb67dd66e3b1caf7ddaba7476b83
6185jcaron <123214297+6185jcaron@users.noreply.github.com>
2023-03-09 19:24:01 -0700

    panel

--- Cubethon/Assets/Scripts/GameManager.cs
+++ Cubethon/Assets/Scripts/GameManager.cs
@@ -1,10 +1,27 @@
 
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-  public void EndGame()
+    bool gameHasEnded = false;
+    public float restartDelay = 1f;
+    public void CompleteLevel()
     {
 
+    }
+  public void EndGame()
+    {
+        if (gameHasEnded == false)
+        {
+            gameHasEnded = true;
+            Debug.Log("GAME OVER");
+            Invoke("Restart", restartDelay);
+        }
+
+    }
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
